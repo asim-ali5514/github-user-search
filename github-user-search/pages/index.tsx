1a4cd2f748026d039fea22fd95c8a35af2eb9fe0
asim-ali5514 <81342795+asim-ali5514@users.noreply.github.com>
2021-09-11 22:36:03 -0700

    Initial Commit

---
+++ github-user-search/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import 'tailwindcss/tailwind.css'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-red-700 text-5xl">hello</h1>
+    </div>
+  )
+}
+
+export default Home
