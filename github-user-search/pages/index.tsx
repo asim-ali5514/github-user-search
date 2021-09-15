eb5dd15db8f18f40db7a6828da2598c55cac9b37
asim-ali5514 <81342795+asim-ali5514@users.noreply.github.com>
2021-09-14 23:05:12 -0700

    Finished a little part of the main component

--- github-user-search/pages/index.tsx
+++ github-user-search/pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import 'tailwindcss/tailwind.css'
 import Navbar from '../components/Navbar'
-import { motion } from "framer-motion"
+import { m, motion } from "framer-motion"
 import search from '../public/icon-search.svg'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
@@ -27,18 +27,44 @@ const Home: NextPage = () => {
     fetchData()
   },[])
 
+  const Months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sept',
+    'Oct',
+    'Nov',
+    'Dec',
+  ]
 
+  const dateString = String(Info.created_at)
+  const fullDate : any = dateString.substring(0, 10)
+  const secondMonth = fullDate[5] + fullDate[6] -1
   return (
   <div className="bg-snow flex h-screen flex-col gap-4  dark:bg-dark-mode-bg transition duration-75">
     <Navbar/>
     <motion.div initial={{y : -150}} animate={{y : 0}} className="flex flex-row items-center px-3 mx-auto gap-2 h-16 rounded-2xl dark:bg-dark-blue bg-white shadow-xl w-full max-w-mob-search">
       <Image src={search} alt="search icon"/>
       <input onChange={(e) => setUser(e.target.value)} type="text" className="bg-transparent outline-none dark:text-white text-sm font-Space-Mono w-full" placeholder="Search GitHub username..." />
-      <button onClick={() => fetchData()} className="h-11 bg-sky-blue w-20 font-Space-Mono text-white px-2 rounded-xl font-medium">Search</button>
+      <motion.button whileHover={{scale : 1.1}} whileTap={{scale : 0.9}} onClick={() => fetchData()} className="h-11 bg-sky-blue w-20 font-Space-Mono text-white px-2 rounded-xl font-medium">Search</motion.button>
     </motion.div>
-    <motion.div className="w-full max-w-mob-search flex flex-row dark:bg-dark-blue rounded-2xl h-mob-container mx-auto">
-      <div>
-        <img src={Info.avatar_url} className="w-mob-profile h-mob-profile rounded-full" alt="profic picture" />
+    <motion.div className="w-full max-w-mob-search shadow-lg flex flex-row dark:bg-dark-blue rounded-2xl h-mob-container mx-auto">
+      <div className="flex flex-row gap-5 ml-6 mt-6">
+        <motion.img initial={{y : -50 , opacity : 0}} animate={{y : 0 , opacity : 1 }} transition={{duration : 0.6}} src={Info.avatar_url} className="w-mob-profile h-mob-profile rounded-full" alt="profic picture" />
+        <div>
+          <motion.h2 initial={{y : -80 , opacity : 0}} animate={{y : 0 , opacity : 1}} transition={{ duration : 0.7}} className="font-Space-Mono font-bold text-base dark:text-white">{Info.name}</motion.h2>
+          <a href={Info.html_url} target="_blank" >
+            <motion.h3 initial={{y : -100 , opacity : 0}} animate={{y : 0 , opacity : 1}} transition={{ duration : 0.9}}  className="font-Space-Mono text-sky-blue">{'@' + Info.login}</motion.h3>
+          </a>
+          <h3 className="font-Space-Mono text-sm dark:text-white text-light-grey">
+            {"Joined" + ' ' + fullDate[8] + fullDate[9] + ' ' + Months[secondMonth] + ' ' + fullDate[0] + fullDate[1] + fullDate[2] + fullDate[3]}
+          </h3>
+        </div>
       </div>
     </motion.div>
     {/* <h1>{Info.name}</h1>
